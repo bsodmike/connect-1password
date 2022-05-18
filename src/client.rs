82b1d777ea99328ab465c81c71f199cf59436ea3
Michael de Silva <michael@mwdesilva.com>
2022-05-18 12:18:44 +0530

    Initial updates to library

---
+++ src/client.rs
@@ -0,0 +1,172 @@
+use crate::error::{ConnectResult, Error, RequestNotSuccessful};
+use exponential_backoff::Backoff;
+use hyper::{
+    client::connect::HttpConnector, header::HeaderValue, Body, Client as HyperClient, Method,
+    Request, Response, StatusCode,
+};
+use hyper_rustls::HttpsConnector;
+use log::{debug, error};
+use serde_json::Value;
+use std::{fmt, ops, thread, time::Duration};
+
+const GET: Method = Method::GET;
+const POST: Method = Method::POST;
+const PUT: Method = Method::PUT;
+
+pub struct Client {
+    api_key: String,
+    http_client: HyperClient<HttpsConnector<HttpConnector>>,
+}
+
+impl Client {
+    pub fn new(api_key: String) -> Self {
+        let https = hyper_rustls::HttpsConnectorBuilder::new()
+            .with_native_roots()
+            .https_only()
+            .enable_http1()
+            .build();
+
+        Self {
+            api_key,
+            http_client: hyper::Client::builder().build::<_, hyper::Body>(https),
+        }
+    }
+
+    pub fn api_key(&self) -> String {
+        self.api_key.clone()
+    }
+
+    pub async fn send_request<T>(
+        &self,
+        method: hyper::Method,
+        endpoint: &str,
+        params: &[(&str, &str)],
+    ) -> Result<(T, Value), Error>
+    where
+        T: serde::de::DeserializeOwned + std::fmt::Debug,
+    {
+        let api_key: &String = &self.api_key;
+
+        let resp = retry_with_backoff(self, &method, &api_key[..], endpoint, params).await?;
+        let status = resp.status();
+
+        let data: (Result<T, Error>, Value) = hyper::body::to_bytes(resp.into_body())
+            .await
+            .map_err(Error::new_network_error)
+            .map(|bytes| {
+                dbg!(&bytes);
+                let json = serde_json::from_slice(&bytes).map_err(Error::new_parsing_error);
+
+                (json, bytes)
+            })
+            .and_then(|data| {
+                let json = data.0;
+                let bytes = std::str::from_utf8(&data.1)?;
+                let json_raw: Value = dbg!(serde_json::from_str(bytes)?);
+
+                dbg!(&json);
+                dbg!(&json_raw);
+
+                match status {
+                    StatusCode::OK => {}
+                    _ => {
+                        debug!(
+                            "Client error! Status: {}, JSON: {}",
+                            status,
+                            &bytes.to_string()
+                        );
+
+                        return Err(RequestNotSuccessful::new(status, bytes.to_string()).into());
+                    }
+                };
+
+                Ok((json, json_raw))
+            })?;
+        let decoded = data.0?;
+        let raw_json = data.1;
+
+        dbg!(&decoded);
+
+        Ok((decoded, raw_json))
+    }
+}
+
+struct RetryErrors<'a>(pub(crate) &'a mut Vec<String>);
+
+impl<'a> fmt::Display for RetryErrors<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.iter().fold(Ok(()), |result, error_msg| {
+            result.and_then(|_| writeln!(f, "{}", error_msg))
+        })
+    }
+}
+
+impl ops::Deref for RetryErrors<'_> {
+    type Target = Vec<String>;
+
+    fn deref(&self) -> &Self::Target {
+        self.0
+    }
+}
+
+/// Attempt exponential backoff when re-attempting requests to the Melissa service.
+async fn retry_with_backoff(
+    client: &Client,
+    method: &hyper::Method,
+    _api_key: &str,
+    _endpoint: &str,
+    params: &[(&str, &str)],
+) -> Result<Response<Body>, Error> {
+    let retries = 1;
+    let min = Duration::from_millis(100);
+    let max = Duration::from_secs(20);
+    let backoff = Backoff::new(retries, min, max);
+    let mut retry_error_messages: Vec<String> = vec![];
+    let mut retry_errors = vec![];
+
+    for duration in &backoff {
+        let url = format!("https://foo?{}", url_encode(params));
+
+        let mut req = hyper::Request::builder()
+            .method(method)
+            .uri(&*url)
+            .body(Body::empty())?;
+
+        req.headers_mut()
+            .insert("Accept", HeaderValue::from_str("application/json")?);
+
+        match client.http_client.request(req).await {
+            Ok(value) => return Ok(value),
+            Err(err) => {
+                let error_message = format!("[ Retrying ]: Client error: {}", err);
+                retry_error_messages.push(error_message);
+                retry_errors.push(err);
+
+                thread::sleep(duration)
+            }
+        }
+    }
+
+    let err = if let Some(val) = retry_errors.pop() {
+        val
+    } else {
+        error!("Unable to unwrap error");
+        return Err(Error::new_internal_error());
+    };
+
+    Err(Error::new_retry_error(err))
+}
+
+fn url_encode(params: &[(&str, &str)]) -> String {
+    params
+        .iter()
+        .map(|&t| {
+            let (k, v) = t;
+            format!("{}={}", k, v)
+        })
+        .fold("".to_string(), |mut acc, item| {
+            acc.push_str(&item);
+            acc.push('&');
+            acc.replace('+', "%2B")
+        })
+}
--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,12 @@
 //! Error and Result module.
 
-use std::{error::Error as StdError, fmt, result::Result};
+use hyper::{header::InvalidHeaderValue, StatusCode};
+use std::{
+    error::Error as StdError,
+    fmt::{self, Display},
+    result::Result,
+    str::Utf8Error,
+};
 
 /// A simple type alias so as to DRY.
 pub type ConnectResult<T> = Result<T, Error>;
@@ -71,6 +77,22 @@ impl Error {
         None
     }
 
+    pub(super) fn new_network_error<E: Into<Cause>>(cause: E) -> Self {
+        Error::new(Kind::NetworkError).with(cause)
+    }
+
+    pub(super) fn new_parsing_error<E: Into<Cause>>(cause: E) -> Self {
+        Error::new(Kind::ParsingError).with(cause)
+    }
+
+    pub(super) fn new_retry_error<E: Into<Cause>>(cause: E) -> Self {
+        Error::new(Kind::RetryError).with(cause)
+    }
+
+    pub(super) fn new_internal_error() -> Self {
+        Error::new(Kind::InternalError)
+    }
+
     /// The error's standalone message, without the message from the source.
     pub fn message(&self) -> impl fmt::Display + '_ {
         self.description()
@@ -78,22 +100,145 @@ impl Error {
 
     fn description(&self) -> &str {
         match self.inner.kind {
-            Kind::NotImplementedError => "Not implemented!",
+            Kind::HyperError(_) => "this is a Hyper related error!",
+            Kind::HyperHttpError(_) => "this is a Hyper HTTP related error!",
+            Kind::InternalError => "internal error",
+            Kind::InvalidHeaderValue => "invalid header value",
+            Kind::NetworkError => "network error",
+            Kind::NotImplementedError => "not implemented error",
+            Kind::ParsingError => "parsing error",
+            Kind::RetryError => "retry error",
+            Kind::RequestNotSuccessful(_) => "client returned an unsuccessful HTTP status code",
+            Kind::SerdeJsonError(_) => "serde deserialization error",
+            Kind::Utf8Error => "parsing bytes experienced a UTF8 error",
         }
+    }
+}
+
+/// Wrapper type which contains a failed request's status code and body.
+#[derive(Debug)]
+pub struct RequestNotSuccessful {
+    /// Status code returned by the HTTP call to the Melissa API.
+    pub status: StatusCode,
+    /// Body returned by the HTTP call to the Melissa API.
+    pub body: String,
+}
+
+impl RequestNotSuccessful {
+    /// Create a new unsuccessful request error.
+    pub fn new(status: StatusCode, body: String) -> Self {
+        Self { status, body }
+    }
+}
+
+impl StdError for RequestNotSuccessful {}
+
+impl Display for RequestNotSuccessful {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "StatusCode: {}, Body: {}", self.status, self.body)
     }
 }
 
 #[derive(Debug)]
 pub(super) enum Kind {
+    /// The failure was due to a Hyper error
+    HyperError(hyper::Error),
+
+    /// The failure was due to a Hyper error
+    HyperHttpError(hyper::http::Error),
+
+    InternalError,
+
+    InvalidHeaderValue,
+
+    /// The failure was due to the network client not working properly.
+    NetworkError,
+
     NotImplementedError,
+
+    ParsingError,
+
+    RetryError,
+
+    RequestNotSuccessful(RequestNotSuccessful),
+
+    SerdeJsonError(serde_json::Error),
+
+    Utf8Error,
 }
 
 impl fmt::Display for Kind {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            &Self::HyperError(_) => {
+                write!(f, "HyperError")
+            }
+            &Self::HyperHttpError(_) => {
+                write!(f, "HyperHttpError")
+            }
+            Self::InternalError => {
+                write!(f, "InternalError")
+            }
+            Self::InvalidHeaderValue => {
+                write!(f, "InvalidHeaderValue")
+            }
+            Self::NetworkError => {
+                write!(f, "NetworkError")
+            }
             Self::NotImplementedError => {
                 write!(f, "NotImplementedError")
             }
+            Self::ParsingError => {
+                write!(f, "ParsingError")
+            }
+            Self::RetryError => {
+                write!(f, "RetryError")
+            }
+            &Self::RequestNotSuccessful(_) => {
+                write!(f, "RequestNotSuccessful")
+            }
+            &Self::SerdeJsonError(_) => {
+                write!(f, "SerdeJsonError")
+            }
+            Self::Utf8Error => {
+                write!(f, "Utf8Error")
+            }
         }
+    }
+}
+
+impl From<hyper::Error> for Error {
+    fn from(err: hyper::Error) -> Self {
+        Error::new(Kind::HyperError(err))
+    }
+}
+
+impl From<hyper::http::Error> for Error {
+    fn from(err: hyper::http::Error) -> Self {
+        Error::new(Kind::HyperHttpError(err))
+    }
+}
+
+impl From<InvalidHeaderValue> for Error {
+    fn from(_err: InvalidHeaderValue) -> Self {
+        Error::new(Kind::InvalidHeaderValue)
+    }
+}
+
+impl From<RequestNotSuccessful> for Error {
+    fn from(err: RequestNotSuccessful) -> Self {
+        Error::new(Kind::RequestNotSuccessful(err))
+    }
+}
+
+impl From<Utf8Error> for Error {
+    fn from(err: Utf8Error) -> Self {
+        Error::new(Kind::Utf8Error).with(err)
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(err: serde_json::Error) -> Self {
+        Error::new(Kind::SerdeJsonError(err))
     }
 }
